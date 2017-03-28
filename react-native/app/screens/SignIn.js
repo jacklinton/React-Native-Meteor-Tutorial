ed3a3c5fd07f0a84cb9ce253b6dea67d782657bf
Jack Linton <jacklinton@mac.com>
2017-03-27 21:42:26 -0400

    Created Sign-Up and Sign-In screens with dynamic state updating in the input fields and the correct keyboard implementation for the particular field. Passwords also come up as dots as they should.

--- react-native/app/screens/SignIn.js
+++ react-native/app/screens/SignIn.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import Container from '../components/Container';
 import { Header } from '../components/Text';
+import { Card } from 'react-native-elements';
+import { Input, PrimaryButton } from '../components/Form';
+import Router from '../config/router';
 
 class SignIn extends Component {
   static route = {
@@ -10,12 +13,37 @@ class SignIn extends Component {
     },
   }
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      emailOrUsername: '',
+      password: '',
+    };
+  }
+
   render() {
     return (
-      <Container>
-        <Header>
-          Sign In
-        </Header>
+      <Container scroll>
+        <Card>
+          <Input
+            label="Email or Username"
+            placeholder="Please enter your email or username..."
+            keyboardType="email-address"
+            onChangeText={(emailOrUsername) => this.setState({ emailOrUsername })}
+            value={this.state.emailOrUsername}
+          />
+          <Input
+            label="Password"
+            placeholder="Please enter your password..."
+            secureTextEntry
+            onChangeText={(password) => this.setState({ password })}
+            value={this.state.password}
+          />
+          <PrimaryButton
+            title="Sign In"
+          />
+        </Card>
       </Container>
     );
   }
--- react-native/app/screens/SignUp.js
+++ react-native/app/screens/SignUp.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react';
 import Container from '../components/Container';
 import { Header } from '../components/Text';
+import { Card } from 'react-native-elements';
+import { Input, PrimaryButton, SecondaryButton } from '../components/Form';
+import Router from '../config/router';
 
 class SignUp extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: '',
+      username: '',
+      password: '',
+      confirmPassword: '',
+    };
+  }
+
   static route = {
     navigationBar: {
       visible: true,
@@ -10,12 +24,61 @@ class SignUp extends Component {
     },
   }
 
+  handleChangeEmail = (email) => {
+    const { username } = this.state;
+    const update = { email };
+    const inferredUsername = email.split('@')[0];
+    if (username === inferredUsername.slice(0, inferredUsername.length - 1)) {
+      update.username = inferredUsername;
+    }
+    this.setState(update);
+  };
+
+  goToSignIn = () => {
+    this.props.navigator.push(Router.getRoute('signIn'));
+  };
+
+
   render() {
     return (
-      <Container>
-        <Header>
-          Sign In
-        </Header>
+      <Container scroll>
+        <Card>
+          <Input
+            label="Email"
+            placeholder="Please enter your email..."
+            keyboardType="email-address"
+            onChangeText={(email) => this.setState({ email })}
+            value={this.state.email}
+          />
+          <Input
+            label="Username"
+            placeholder="Please enter your username..."
+            onChangeText={(username) => this.setState({ username })}
+            value={this.state.username}
+          />
+          <Input
+            label="Password"
+            placeholder="Please enter your password..."
+            secureTextEntry
+            onChangeText={(password) => this.setState({ password })}
+            value={this.state.password}
+          />
+          <Input
+            label="Confirm Password"
+            placeholder="Please enter confirm your password..."
+            secureTextEntry
+            onChangeText={(confirmPassword) => this.setState({ confirmPassword })}
+            value={this.state.confirmPassword}
+          />
+          <PrimaryButton
+            title="Sign Up"
+          />
+        </Card>
+
+        <SecondaryButton
+          title="Sign In"
+          onPress={this.goToSignIn}
+        />
       </Container>
     );
   }
